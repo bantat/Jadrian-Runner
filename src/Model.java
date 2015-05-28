ab9498a02544d7157607ab0af6bd9b8f8e0da32d
Ben <withbroeb@carleton.edu>
2015-05-27 21:50:39 -0500

    Add some high level method signatures for Model

--- src/Model.java
+++ src/Model.java
@@ -1,7 +1,55 @@
+import javafx.stage.Stage;
+
 /**
  * Created by torebanta on 5/27/15.
+ * @author Tore Banta
+ * @author Ben Withbroe
  */
 
 public class Model {
+    Stage gameWindow;
 
+    public void Model(Stage gameWindow) {
+        gameWindow = gameWindow;
+    }
+
+    /**
+     * Updates the positions of each of the moving sprites currently on the
+     * screen, based on the amount of time passed since the last calculation.
+     * @param dTime the amount of time since the last position calculation
+     */
+    public void updatePositions(int dTime) {
+
+    }
+
+    /**
+     * Updates the player's position based on the amount of time passed since
+     * the last calculation.
+     * @param dTime the amount of time since the last position calculation
+     */
+    private void updateSpritePosition(int dTime, Sprite sprite) {
+
+    }
+
+    /**
+     * Makes the player jump if doing so is valid.
+     */
+    public void jump() {
+
+    }
+
+    /**
+     * Checks if it is valid for the player to jump.
+     * @return True if it is valid for the player to jump.
+     */
+    private boolean canJump() {
+        return true;
+    }
+
+    /**
+     * Generates the next section of track for the user to play on.
+     */
+    public void genNewTrack() {
+
+    }
 }
