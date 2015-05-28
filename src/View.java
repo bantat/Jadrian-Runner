92154678acc869a46da211fac6e5a0f7e60cd5df
Tore Banta <torebanta@eduroam-160-90.dyn.carleton.edu>
2015-05-27 21:26:00 -0500

    Added constructor for View, some basic implementation for Main

--- src/View.java
+++ src/View.java
@@ -1,6 +1,22 @@
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+
 /**
  * Created by torebanta on 5/27/15.
  */
 
 public class View {
+    private Model model;
+    private Canvas canvas;
+    private GraphicsContext context;
+
+    public View(Canvas gameCanvas, Model gameModel) {
+        canvas = gameCanvas;
+        model = gameModel;
+        context = canvas.getGraphicsContext2D();
+    }
+
+    public void drawGameScreen() {
+        //...
+    }
 }
