f08d3966926cdf48d16ce96e3252a7d7a4687967
Ben <withbroeb@carleton.edu>
2015-05-28 20:38:52 -0500

    Initial commit for ViewSprite class

--- src/ModelSprite.java
+++ src/ModelSprite.java
@@ -1,5 +1,29 @@
+import java.util.List;
+import java.util.ArrayList;
+
 /**
  * Created by Ben on 5/28/2015.
  */
-public class ModelSprite {
+public class ModelSprite extends Sprite{
+    private List<Integer> position = new ArrayList<Integer>();
+    private ViewSprite viewSprite;
+
+//    public void Sprite() {
+//
+//    }
+
+    /**
+     * Sets the current position of the sprite within the cannonical Model
+     * coordinates.
+     * @param x the x coordinate
+     * @param y the y coordinate
+     */
+    @Override
+    public void setPosition(int x, int y) {
+        position = new ArrayList<Integer>();
+        position.add(x);
+        position.add(y);
+
+        viewSprite.
+    }
 }
---
+++ src/ViewSprite.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Ben on 5/28/2015.
+ */
+public class ViewSprite {
+}
