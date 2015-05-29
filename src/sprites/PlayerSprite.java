f8efd2db46f1642afa411ecafa9b2745b4970268
Ben <withbroeb@carleton.edu>
2015-05-28 21:33:06 -0500

    Initial commit for PlayerViewSprite class

--- src/sprites/PlayerSprite.java
+++ src/sprites/PlayerSprite.java
@@ -4,6 +4,11 @@
 public class PlayerSprite extends ModelSprite {
     private int runState = 0;
 
+    public PlayerSprite(PlayerViewSprite viewSprite) {
+        super(viewSprite);
+        )
+    }
+
     public void animateRun() {
         if (runState == 0) {
             runState = (runState + 1) % 2;
