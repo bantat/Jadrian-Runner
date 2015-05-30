47015a9a5df19686474e20e583d7d0075e7ab6e1
Alex Griese <griesea@carleton.edu>
2015-05-29 22:04:46 -0500

    Added Obstacle class.

--- src/sprites/Obstacle.java
+++ src/sprites/Obstacle.java
@@ -1,7 +1,18 @@
 package sprites;
 
+import sprites.GameObject;
+
+import java.awt.*;
+
 /**
  * Created by alexgriese on 5/29/15.
  */
-public class Obstacle {
+public class Obstacle extends GameObject {
+
+
+    public void update() {
+
+    }
+
+    public void draw(Canvas gameCanvas){}
 }
