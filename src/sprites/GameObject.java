74b0c5a0ad405db09ad69d7571acdf24b9f76fc0
Alex Griese <griesea@carleton.edu>
2015-06-04 18:27:14 -0500

    Commented GameObject.java

--- src/sprites/GameObject.java
+++ src/sprites/GameObject.java
@@ -38,7 +38,6 @@ public abstract class GameObject {
     /**
      * Method for getting an object representation of the GameObject's collision
      * box.
-     *
      * @return Rectangle2D hitBox
      */
     public Rectangle2D getHitBox() {
@@ -48,7 +47,6 @@ public abstract class GameObject {
 
     /**
      * Method for checking if two GameObjects hitboxes intersect.
-     *
      * @param otherObject
      * @return boolean isCollsion
      */
@@ -58,18 +56,34 @@ public abstract class GameObject {
         return thisHitBox.intersects(otherHitBox);
     }
 
+    /**
+     * Gets the x position of the GameObject.
+     * @return the x position of the GameObject
+     */
     public int getX() {
         return (int) x;
     }
 
+    /**
+     * Gets the y position of the GameObject.
+     * @return the y position of the GameObject
+     */
     public int getY() {
         return (int) y;
     }
 
+    /**
+     * Gets the width of the GameObject.
+     * @return the width of the GameObject
+     */
     public int getWidth() {
         return width;
     }
 
+    /**
+     * Gets the height of the GameObject.
+     * @return the height of the GameObject
+     */
     public int getHeight() {
         return height;
     }
@@ -90,28 +104,47 @@ public abstract class GameObject {
 //        down = isDown;
 //    }
 
+    /**
+     * Sets the x and y position of the GameObject.
+     * @param x the new x position of the GameObject.
+     * @param y the new y position of the GameObject.
+     */
     public void setPosition(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * Sets the x and y velocity of the GameObject.
+     * @param dx the new x velocity of the GameObject.
+     * @param dy the new y velocity of the GameObject.
+     */
     public void setDirection(double dx, double dy) {
         this.dx = dx;
         this.dy = dy;
     }
 
+    /**
+     * Gets the x velocity of the GameObject.
+     * @return the x velocity of the GameObject
+     */
     public double getDirectionX() {
         return this.dx;
     }
+
+    /**
+     * Gets the y velocity of the GameObject.
+     * @return the y velocity of the GameObject
+     */
     public double getDirectionY() {
         return this.dy;
     }
 
     /**
      * Method for checking if a GameObject is within the boundaries of a Canvas.
-     *
-     * @param gameCanvas
-     * @return boolean isOffScreen
+     * @param gameCanvas the canvas the GameObject has been drawn on
+     * @return boolean isOffScreen true, if the object is off the screen;
+     *                             false, if the object is on the screen
      */
     public boolean isOffScreen(Canvas gameCanvas) {
         if (x + width < 0) return true;
@@ -121,8 +154,16 @@ public abstract class GameObject {
         else return false;
     }
 
+    /**
+     * An abstract method implemented by implementations of the GameObject
+     * class. Will update the GameObjects position.
+     */
     public abstract void updatePosition();
 
+    /**
+     * Gets the SpriteAnimation.
+     * @return the sprite animation.
+     */
     public SpriteAnimation getAnimation() {
         return animation;
     }
