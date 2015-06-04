4e6803290c1949f5c38905b72312548c39ad72b8
Alex Griese <griesea@carleton.edu>
2015-06-04 18:29:25 -0500

    Commented Obstacle.java

--- src/sprites/Obstacle.java
+++ src/sprites/Obstacle.java
@@ -5,6 +5,10 @@ package sprites;
  */
 public class Obstacle extends GameObject {
 
+    /*
+    Takes and sets the width, height, speed and x,y position of the given
+    object. Inherits? the methods of the GameObject abstract class. Se
+     */
     public Obstacle(int width, int height, int speed, int x, int y) {
 
         super();
@@ -17,6 +21,9 @@ public class Obstacle extends GameObject {
 
     }
 
+    /**
+     * Updates the position of the Obstacle object based on its speed.
+     */
     public void updatePosition() {
         x = x + dx;
     }
