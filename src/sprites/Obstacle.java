4c01e2f66a9d341cde6c2bc3361b156d18d7848c
Alex Griese <griesea@carleton.edu>
2015-06-04 20:17:57 -0500

    Fixed comment syntax.

--- src/sprites/Obstacle.java
+++ src/sprites/Obstacle.java
@@ -5,10 +5,8 @@ package sprites;
  */
 public class Obstacle extends GameObject {
 
-    /*
-    Takes and sets the width, height, speed and x,y position of the given
-    object. Inherits? the methods of the GameObject abstract class. Se
-     */
+    // Takes and sets the width, height, speed and x,y position of the given
+    // object. Inherits? the methods of the GameObject abstract class.
     public Obstacle(int width, int height, int speed, int x, int y) {
 
         super();
